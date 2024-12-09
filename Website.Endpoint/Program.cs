a05341c4421aff59524005d9ffadf6ffb49b91f9
AfsanehSharafi <Ashdev.ir@gmail.com>
2024-12-09 19:55:35 +0330

    Add project files.

---
+++ Website.Endpoint/Program.cs
@@ -0,0 +1,37 @@
+using Application.Interfaces.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Contenxts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+#region ConnectinString
+var connnection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseSqlServer(connnection));
+
+builder.Services.AddDbContext<IdentityDatabaseContext>(options => options.UseSqlServer(connnection));
+#endregion
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
