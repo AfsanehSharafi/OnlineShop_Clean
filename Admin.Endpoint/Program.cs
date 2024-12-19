ea333a4264f27dfa2948cab9b37cfbcd90333082
AfsanehSharafi <Ashdev.ir@gmail.com>
2024-12-19 23:14:38 +0330

    Visitor-3

--- Admin.Endpoint/Program.cs
+++ Admin.Endpoint/Program.cs
@@ -1,7 +1,10 @@
+using Application.Visitors.GetTodayReport;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddTransient<IGetTodayReportService, GetTodayReportService>();
 
 var app = builder.Build();
 
