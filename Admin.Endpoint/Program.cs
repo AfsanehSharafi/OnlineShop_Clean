eab00c340a05ba7442f05cf9aa7d03751eef0c68
AfsanehSharafi <Ashdev.ir@gmail.com>
2024-12-20 13:10:42 +0330

    Visitor-4

--- Admin.Endpoint/Program.cs
+++ Admin.Endpoint/Program.cs
@@ -1,10 +1,13 @@
+using Application.Interfaces.Contexts;
 using Application.Visitors.GetTodayReport;
+using Persistence.Contenxts.MongoContext;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddTransient<IGetTodayReportService, GetTodayReportService>();
+builder.Services.AddTransient(typeof(IMongoDbContext<>), typeof(MongoDbContext<>));
 
 var app = builder.Build();
 
